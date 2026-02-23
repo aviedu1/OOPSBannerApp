bc12658cc91f656660b0e11fb6c00b8180cfcdcf
aviedu1 <ar7806@srmist.edu.in>
2026-02-23 11:11:25 +0530

    UC2: Completed Banner using print and concatenation

---
+++ OOPSBannerApp.java
@@ -0,0 +1,7 @@
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        // Line 1
+        System.out.println(" **** " + " " + " **** " + " " + " **** " + " " + " **** ");
+        
+    }
+}
