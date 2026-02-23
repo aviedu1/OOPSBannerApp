1460e26e79f1be63e8f6b58de43854f8044ce3e1
aviedu1 <ar7806@srmist.edu.in>
2026-02-23 11:17:56 +0530

    UC3: Updated banner to use String.join for efficiency

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,7 +1,14 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
         // Line 1
-        System.out.println(" **** " + " " + " **** " + " " + " **** " + " " + " **** ");
+       System.out.println(String.join("", " **** ", " ", " **** ", " ", " ***** ", " ", " **** "));
+        
+        // Line 2:
+        System.out.println(String.join("", " ** **", " ", " ** **", " ", " ** **", " ", " ** "));
+        
+        // ... (Update all 7 lines similarly)
+        
+        System.out.println(String.join("", " **** ", " ", " **** ", " ", " ** ", " ", " **** "));
         
     }
 }
